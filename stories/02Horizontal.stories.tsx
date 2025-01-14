9df655512a24ce51bfa0bcbb4734c582ce065be0
Jawaad Khan <Jaw_khan@hotmail.co.uk>
2025-01-14 20:56:50 +0000

    🔥 remove unused button

--- stories/02Horizontal.stories.tsx
+++ stories/02Horizontal.stories.tsx
@@ -1,41 +1,32 @@
-import React, { useState } from 'react';
-import { Button } from '@storybook/react/demo';
-import SplitPane, { Pane } from '../src';
-import '../src/themes/default.scss';
+import React, { useState } from "react";
+import SplitPane, { Pane } from "../src";
+import "../src/themes/default.scss";
 
 export default {
-    title: 'Basic',
+  title: "Basic",
 };
 
 export const BasicHorizontal = () => {
-    const [sizes, setSizes] = useState<(number | string)[]>([
-        100,
-        200,
-        'auto',
-    ]);
+  const [sizes, setSizes] = useState<(number | string)[]>([100, 200, "auto"]);
 
-    const layoutCSS = {
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    };
+  const layoutCSS = {
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
 
-    return (
-        <div style={{ height: 500 }}>
-            <p>Set size ='horizontal ', switch to horizontal panel</p>
-            <SplitPane
-                split='horizontal'
-                sizes={sizes}
-                onChange={(sizes) => setSizes(sizes)}
-            >
-                <div style={{ ...layoutCSS, background: '#ddd' }}>
-                    pane1
-                </div>
-                <div style={{ ...layoutCSS, background: '#c0c3c6' }}>
-                    pane2
-                </div>
-            </SplitPane>
-        </div>
-    );
+  return (
+    <div style={{ height: 500 }}>
+      <p>Set size ='horizontal ', switch to horizontal panel</p>
+      <SplitPane
+        split="horizontal"
+        sizes={sizes}
+        onChange={(sizes) => setSizes(sizes)}
+      >
+        <div style={{ ...layoutCSS, background: "#ddd" }}>pane1</div>
+        <div style={{ ...layoutCSS, background: "#c0c3c6" }}>pane2</div>
+      </SplitPane>
+    </div>
+  );
 };
